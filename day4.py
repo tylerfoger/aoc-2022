3365102b360e3302cc4c70d2b42eb92c322d7df9
Tyler Foger <tylerfoger@gmail.com>
2023-06-07 22:10:47 -0700

    complete day4 python

---
+++ day4.py
@@ -0,0 +1,31 @@
+def makeSectionList(assignments):
+    start = int(assignments[0])
+    end = int(assignments[1])
+    listOfSections = []
+    for section in range(start, end + 1):
+        listOfSections.append(section)
+    return listOfSections
+
+
+def fullyContainedPairs():
+    listOfPairs = 0
+    with open('day4-input.txt', 'r') as f:
+        for line in f.readlines():
+            splitLines = line.split(',')
+            elf1 = makeSectionList(splitLines[0].split('-'))
+            elf2 = makeSectionList(splitLines[1].split('-'))
+
+            sameSections = set(elf1).intersection(elf2)
+            if len(sameSections) < 1:
+                pass
+            elif len(elf1) - len(sameSections) == 0:
+                listOfPairs += 1
+            elif len(elf2) - len(sameSections) == 0:
+                listOfPairs += 1
+            else:
+                pass
+    return listOfPairs
+
+
+totalPairs = fullyContainedPairs()
+print(totalPairs)
